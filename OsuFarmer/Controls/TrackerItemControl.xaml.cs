b885479abe8c7e7e6b8af2a882d7371d46747ee1
Mark Meijer <engineermarkpro@gmail.com>
2022-02-12 00:34:25 +0100

    show cadet for positive/negative gains

--- OsuFarmer/Controls/TrackerItemControl.xaml.cs
+++ OsuFarmer/Controls/TrackerItemControl.xaml.cs
@@ -2,27 +2,50 @@ namespace OsuFarmer;
 
 public partial class TrackerItemControl : ContentView
 {
-	public string AttachedProperty { get; set; } = "null";
+    public string AttachedProperty { get; set; } = "null";
+    public bool InvertColors = false;
 
-	public void SetTitle(string title)
-	{
-		TitleElement.Text = title;
-	}
+    private static readonly Color ColorNegative = Colors.Red;
+    private static readonly Color ColorPositive = Colors.LightGreen;
+    private static readonly Color ColorNeutral = Colors.White;
 
-	public void SetCurrentValue(double value)
-	{
-		CurrentValueElement.Text = string.Format("{0:n0}", value);
-		//CurrentValue = string.Format("{0:n0}", value);
-	}
+    public static readonly BindableProperty ChangedTextColorProperty = BindableProperty.Create(nameof(ChangedTextColor), typeof(Color), typeof(TrackerItemControl), Colors.White);
 
-	public void SetChangedValue(double value)
-	{
-		ChangedValueElement.Text = string.Format("{0:n0}", value);
-	}
+    public Color ChangedTextColor
+    {
+        get => (Color)GetValue(TrackerItemControl.ChangedTextColorProperty);
+        set => SetValue(TrackerItemControl.ChangedTextColorProperty, value);
+    }
 
-	public TrackerItemControl()
-	{
-		InitializeComponent();
-		BindingContext = this;
-	}
+    public void SetTitle(string title)
+    {
+        TitleElement.Text = title;
+    }
+
+    public void SetCurrentValue(double value)
+    {
+        CurrentValueElement.Text = string.Format("{0:n0}", value);
+        //CurrentValue = string.Format("{0:n0}", value);
+    }
+
+    public void SetChangedValue(double value)
+    {
+        if (value == 0)
+            ChangedValueSignElement.Text = string.Empty;
+        else if (value > 0)
+            ChangedValueSignElement.Text = "\x2227";
+        else
+            ChangedValueSignElement.Text = "\x2228";
+
+        ChangedValueElement.Text = string.Format("{0:n0}", value);
+        ChangedTextColor = !InvertColors ?
+            (value == 0 ? ColorNeutral : (value > 0 ? ColorPositive : ColorNegative)) :
+            (value == 0 ? ColorNeutral : (value > 0 ? ColorNegative : ColorPositive));
+    }
+
+    public TrackerItemControl()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
 }
